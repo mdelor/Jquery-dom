7d5bf78f33a4657f33a41777c17377fb6c182a22
mdelor <116556228+mdelor@users.noreply.github.com>
2022-12-08 13:25:39 +0100

    cleaning

--- dom.js
+++ dom.js
@@ -1,12 +1,16 @@
 $(function () {
+
+    //Removal
     $(".detach").click(function () {
         $("#detach").children().first().detach();
         
     });
+
     $(".empty").click(function () {
         $("#empty").children().first().empty();
         
     });
+
     $(".remove").click(function () {
         $("#remove").children().first().remove();
         //Préciser la différence avec detach
@@ -16,10 +20,12 @@ $(function () {
         $("#unwrap").children().unwrap();
         
     });
+    // Replacement
     $(".replaceAll").click(function () {
         $("<p>Boîtes Remplacées</p>").replaceAll( $("#replaceAll").children() );
         
     });
+
     $(".replaceWith").click(function () {
         $("#replaceWith").children().replaceWith("<p>Boîtes remplacées</p>");
         
