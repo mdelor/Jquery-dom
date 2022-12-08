2193d412d83f1685115fc77abc5aee6bc83e9096
mdelor <116556228+mdelor@users.noreply.github.com>
2022-12-08 12:16:57 +0100

    init

---
+++ dom.js
@@ -0,0 +1,28 @@
+$(function () {
+    $(".detach").click(function () {
+        $("#detach").children().first().detach();
+        
+    });
+    $(".empty").click(function () {
+        $("#empty").children().first().empty();
+        
+    });
+    $(".remove").click(function () {
+        $("#remove").children().first().remove();
+        //Préciser la différence avec detach
+    });
+    
+    $(".unwrap").click(function () {
+        $("#unwrap").children().unwrap();
+        
+    });
+    $(".replaceAll").click(function () {
+        $("<p>Boîtes Remplacées</p>").replaceAll( $("#replaceAll").children() );
+        
+    });
+    $(".replaceWith").click(function () {
+        $("#replaceWith").children().replaceWith("<p>Boîtes remplacées</p>");
+        
+    });
+
+});
